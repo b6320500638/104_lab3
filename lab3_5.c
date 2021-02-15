058dcbf8faf1fe509a42b0885c13d0a96a87e26f
b6320500638 <worakit.i@ku.th>
2021-02-15 17:57:07 +0700

    อาจารย์ครับทำไมข้อนี้ผมถึงได้0หรอครับหรือว่าcodeผมเลอะไป

---
+++ lab3_5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int k,i=2,j=0,x=0,y=0,n;
+    scanf("%d",&k);
+    if(k>=0&&k<=10000)
+    {
+    n=k;
+    while(k>1)
+    {
+            if(k%i==0)
+            {
+                if(x==i)
+                {
+                    y=1;
+                }
+                k=k/i;
+                x=i;
+                i=2;
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+
+    }
+    if(j!=3||y)
+            {printf("%d is not a Lucky Number.",n);}
+    else if(j==3)
+            {printf("%d is a Lucky Number.",n);}
+    }
+}
